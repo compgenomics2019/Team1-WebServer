a569e6cdc572c2b78e989a00852190a376f9e982
hanying <hpan48@gatech.edu>
2019-04-18 14:48:33 -0400

    file stream

--- app/Http/Controllers/Home/FileManagerController.php
+++ app/Http/Controllers/Home/FileManagerController.php
@@ -40,7 +40,9 @@ class FileManagerController extends Controller
         if (substr_compare($newFileName, '.fastq', $file_name_len-6) != 0){
             return redirect("FileManager/badname");
         }
-        $file->storePubliclyAs($category, $newFileName, ['disk' => 'uploads']); // todo: save large files
+        $disk = Storage::disk('uploads');
+        $disk -> putFileAs($category, fopen($file, 'r+'), $newFileName);
+//        $file->storePubliclyAs($category, $newFileName, ['disk' => 'uploads']); // todo: save large files
         $output = exec('../../t1g5/bin/python3 ../scripts/filecheck.py ../storage/app/uploads/'.$category.'/'.$newFileName);
         if ($output == "fail") {
             Storage::delete($category . "/" . $newFileName);
