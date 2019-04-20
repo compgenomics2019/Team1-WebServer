3c693604018def2b729fdd7a953513bcb8a645e8
hanying <hpan48@gatech.edu>
2019-04-20 13:43:13 -0400

    catch err

--- scripts/main.py
+++ scripts/main.py
@@ -314,8 +314,7 @@ def run_trim(trimmomatic_jar, _input_files, _tmp_dir, window, threshold, headcro
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     proc.wait()
     out, trim_summary = proc.communicate()
-    print(trim_summary)
-    return 0
+    print("----TRIM----", out, trim_summary)
     if trim_summary:
         trim_summary = trim_summary.decode("utf-8")
         drop_rate = trim_summary.split("\n")[-3].split()[-1][1:-2]
