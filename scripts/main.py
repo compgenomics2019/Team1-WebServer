576518a784338c5ff7d2e9613a05995b59a0b31c
hanying <hpan48@gatech.edu>
2019-04-21 18:03:24 -0400

    remove tmp

--- scripts/main.py
+++ scripts/main.py
@@ -402,11 +402,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     tmp = "../storage/app/public/" + args.j
-    if not os.path.exists(tmp):
-        os.mkdir(tmp)
-    else:
-        shutil.rmtree(tmp)
-        os.mkdir(tmp)
+    os.mkdir(tmp)
+    # if not os.path.exists(tmp):
+    #     pass
+    # else:
+    #     shutil.rmtree(tmp)
+    #     os.mkdir(tmp)
 
     if args.a:
         trim_files(args.infastq, tmp, "../../team1tools/GenomeAssembly/Trimmomatic-0.36/trimmomatic-0.36.jar")
@@ -431,4 +432,5 @@ if __name__ == "__main__":
             vfdb(in_annotation_fna, "../storage/app/uploads/annotation/%s.gff"%args.j)
     if args.d:
         pass
+    subprocess.call(['rm', "-rf", tmp])
     print("main.py finished!")
