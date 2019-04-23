9528a96ba05bc16a5c4651641982e2150d39759e
hanying <hpan48@gatech.edu>
2019-04-23 02:18:56 -0400

    meg

--- scripts/main.py
+++ scripts/main.py
@@ -430,6 +430,33 @@ def MASH(path, job):
         print(idx, file, mash_out, sep="-" * 5)
     isolates_df.to_csv(tmp + "/mash.csv", header=True, index=True)
 
+    with open(tmp + '/mummer.meg', 'w') as f:
+        txt = '#mega\n'
+        txt += '!Title: Genetic distance data of N meningitidis strains;\n'
+        txt += '!Format DataType=Distance DataFormat=LowerLeft NTaxa=50;\n'
+        txt += '!Description\n'
+        txt += 'Genetic distance data of N meningitidis strains based on predicted cgMLST;\n\n'
+        with open(tmp + "/mash.csv", "r") as fp:
+            count = 0
+            j = 0
+            for line in fp:
+                if count == 0:
+                    count += 1
+                    contigs = line.strip().split(',')[1:]
+                    contigs = [contig.split('.')[0] for contig in contigs]
+                    contigs = [contig.split('_')[0] for contig in contigs]
+                    for contig in contigs:
+                        txt += '#' + contig + '\n'
+                    txt += '\n'
+                else:
+                    string = ''
+                    for i in range(j):
+                        string += line.strip().split(',')[1:][i] + '\t'
+                    txt += string + '\n'
+                    j += 1
+
+        f.write(txt)
+
 
 def calDifference1(inFile):
     """ before calculate difference for output of cgMLST, the last two columns need to be cut
